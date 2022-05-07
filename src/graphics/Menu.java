875a9c81b733c3242ed805f5000c846f62462be8
pedrooouo Alves <pedroaugustoba@hotmail.com>
2022-05-07 19:30:31 -0300

    Alteracoes Pedro 2

--- src/graphics/Menu.java
+++ src/graphics/Menu.java
@@ -6,6 +6,8 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -14,10 +16,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.*;
@@ -29,7 +30,13 @@ import java.awt.Insets;
 import teste.Aplicacao;
 
 public class Menu implements ActionListener {
-    
+
+    private static final int MARGENS = 300;
+    private static final int ESPACAMENTO = 30;
+    private static final int CABECALHO = 300;
+    private static final int RODAPE = 100;
+
+
     private JButton JButtonPlay;
     private JButton JButtonRanking;
     private JButton JButtonOpcoes;
@@ -47,64 +54,100 @@ public class Menu implements ActionListener {
         janela.setLayout(new GridLayout(0, 1));
         BufferedImage ImagemdeFundo=null;
         
+        
         try {
+            //LABEL
+            //Criando Label com a Imagem de Fundo
             ImagemdeFundo = ImageIO.read(new FileInputStream("src/img/background/TetrisMenu.png"));
             JLabel label = new JLabel();
             label.setIcon(new ImageIcon(new ImageIcon(ImagemdeFundo).getImage().getScaledInstance(800, 600, ImagemdeFundo.SCALE_DEFAULT)));
             label.setLayout( new FlowLayout() );
-
-
             janela.add(label);
+            //Layout de GridBagConstrains para Organizarmos os Botões
             label.setLayout(new GridBagLayout());
             GridBagConstraints c = new GridBagConstraints();
 
-        JPanel panelPlay = new JPanel();
-        JButtonPlay = new JButton("Play!");
+        //BOTÃO PLAY
+        JButtonPlay = new JButton();
+        //Posição na Matrix
         c.gridx = 0;
         c.gridy = 0;
+        //Tamanho
         c.gridwidth=2;
+        c.weightx=0.1;
+        //c.weighty=0.5;
         c.fill = GridBagConstraints.HORIZONTAL;
-        //Código para os Panels ocuparem todo o espaço disponível
-        c.insets = new Insets(250,300,0,300);
-        c.weightx=1;
-        c.weighty=1;
-        c.fill=GridBagConstraints.BOTH;
-        JButtonPlay.setBackground(Color.green);
+        //Espaçamento
+        c.insets = new Insets(CABECALHO,MARGENS,0,MARGENS);
+        //c.weightx=0.5;
+        //c.weighty=0.2;
+        //c.fill=GridBagConstraints.BOTH;
+        JButtonPlay.setBackground(Color.gray);
         label.add(JButtonPlay, c);
+        JButtonPlay.setActionCommand("iniciar");
+        JButtonPlay.addActionListener(this);
+        //carregamento do ícone
+        //ImageIcon playicon = new ImageIcon("src/img/buttons/PlayBotao.png");
+        //JButtonPlay.setIcon(playicon);
 
-        JPanel panelRanking = new JPanel();
-        JButtonRanking = new JButton("Ranking!");
+        //BOTÃO RANKING
+        JButtonRanking = new JButton();
         c.gridx = 0;
         c.gridy = 1;
         c.gridwidth=2;
-        c.insets = new Insets(50,300,0,300);
+        c.insets = new Insets(ESPACAMENTO,MARGENS,0,MARGENS);
+        JButtonRanking.setBackground(Color.gray);
         label.add(JButtonRanking, c);
+        JButtonRanking.setActionCommand("ranking");
+        JButtonRanking.addActionListener(this);
+        
 
+        //BOTÃO OPÇÕES
         JButtonOpcoes = new JButton("Opções!");
         c.gridx = 0;
         c.gridy = 3;
         c.gridwidth=1;
-        c.insets = new Insets(50,300,100,0);
+        c.insets = new Insets(ESPACAMENTO,MARGENS,RODAPE,0);
         label.add(JButtonOpcoes, c);
 
+        //BOTÃO SAIR
         JButtonSair = new JButton("Fechar");
         c.gridx = 1;
         c.gridy = 3;
         c.gridwidth=1;
-        c.insets = new Insets(50,0,100,300);
+        c.insets = new Insets(ESPACAMENTO,0,RODAPE,MARGENS);
         label.add(JButtonSair, c);
-
-
-        JButtonPlay.setActionCommand("iniciar");
-		JButtonRanking.setActionCommand("ranking");
-		 
-		JButtonPlay.addActionListener(this);
-		JButtonRanking.addActionListener(this);
         
+        
+
+        
+
         janela.add(label);
         janela.pack();
 		janela.setLocationRelativeTo(null);
 		janela.setVisible(true);
+
+        System.out.println(JButtonPlay.getWidth());
+        System.out.println(JButtonPlay.getHeight());
+
+        JButtonPlay.setHorizontalTextPosition(JButton.CENTER);
+        JButtonPlay.setVerticalTextPosition(JButton.CENTER);
+        JButtonPlay.setMargin(new Insets(0,0,0,0));
+        ImageIcon playicon = new ImageIcon("src/img/buttons/PlayBotao.png");
+        JButtonPlay.setIcon(playicon);
+        //JButtonPlay.setPreferredSize(new Dimension(220, 5));
+
+        //Botão 2
+        //carregamento do ícone
+        ImageIcon rankingicon = new ImageIcon("src/img/buttons/RankingBotao.png");
+        JButtonRanking.setIcon(rankingicon);
+        JButtonRanking.setMargin(new Insets(0,0,0,0));
+
+        System.out.println(JButtonPlay.getWidth());
+        System.out.println(JButtonPlay.getHeight());
+
+        
+        
         
     } catch (FileNotFoundException e) {
         e.printStackTrace();
